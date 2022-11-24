8fcd68e6a9fb7484cb6370c48a8f2e078a87a2c6
antoniaarga <maargandonaa@gmail.com>
2022-11-24 08:37:16 -0300

    header seccion home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import "./home.css";
+import React from "react";
+import logo from "../assets/logo.png";
+import Button from 'react-bootstrap/Button';
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="header">
+      <div className="logo-section">
+      <img className="logo" src={logo} alt="Logo" />
+      <p> Mejora tus hábitos, mejora el mundo, mejora tu paladar. Prueba Nigiri y enamórate otra vez.</p>
+      <Button variant="dark">Menú</Button>
+      </div>
+    </div>
+    
+  );
+};
 
 export default Home
